a9b471d6566f270a3475bef721ad89b418c14fbb
Sandeepchitukula <146516018+Sandeepchitukula@users.noreply.github.com>
2026-07-23 13:48:55 +0530

    Create Badge.tsx

---
+++ components/Badge.tsx
@@ -0,0 +1,7 @@
+export default function Badge({ value, prefix = '' }: { value: string; prefix?: string }) {
+  return (
+    <span className={`${prefix}${value} inline-block px-2 py-0.5 rounded-full text-xs font-medium capitalize`}>
+      {value.replace('_', ' ')}
+    </span>
+  );
+}
